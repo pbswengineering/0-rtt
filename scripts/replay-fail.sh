8b89a05b1edab4c7a88f5afef8f0de5c180233a1
Paolo Bernardi <paolo@bernardi.cloud>
2024-01-08 13:45:27 +0100

    Removed unused script

--- scripts/replay-fail.sh
+++
@@ -1,6 +0,0 @@
-RED='\033[0;31m'
-WHITE='\033[1;37m'
-GREEN='\033[1;32m'
-NC='\033[0m'
-echo -e "${WHITE}0-RTT 'classic' replay attack against a ${GREEN}protected${WHITE} server...${NC}"
-python tls_playback.py --host 127.0.0.1 --port 444 --mode=no_protections
